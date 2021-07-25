2a2c1d51faad08655bc30e04e3195b72b7e3361f
Dheeraj R <83569971+DheerajR1@users.noreply.github.com>
2021-07-26 01:24:39 +0530

    init: final;

---
+++ app.js
@@ -0,0 +1,37 @@
+const cS1 = document.querySelector(".carousel-slides");
+const csI = document.querySelectorAll(".carousel-slides img");
+
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+let counter = 1;
+const size  = csI[0].clientWidth;
+
+cS1.style.transform = `translateX(`+(-size * counter) + `px)`;
+
+nextBtn.addEventListener('click', () => {
+    if (counter >= csI.length-1) return;
+    cS1.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    cS1.style.transform = `translateX(`+(-size * counter) + `px)`;
+});
+
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0) return;
+    cS1.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    cS1.style.transform = `translateX(`+(-size * counter) + `px)`;
+});
+
+cS1.addEventListener('transitionend',() => {
+    if (csI[counter].id == "lastImg") {
+        cS1.style.transition = `none`;
+        counter = csI.length -2;
+        cS1.style.transform = `translateX(`+(-size * counter) + `px)`;
+    }
+    if (csI[counter].id == "firstImg") {
+        cS1.style.transition = `none`;
+        counter = csI.length - counter;
+        cS1.style.transform = `translateX(`+(-size * counter) + `px)`;
+    }
+})
